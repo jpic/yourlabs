1c483a38374453593dcaeb710aff91ec964003f8
James Pic <jamespic@gmail.com>
2011-12-07 10:40:14 +0100

    Move exchange app to admin_hack app

--- yourlabs/exchange/settings.py
+++
@@ -1,5 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-EXCHANGE_PARSER_CHOICES = (
-    ('exchange.parsers.CsvParser', _('CSV')),
-)
--- yourlabs/exchange/utils.py
+++
@@ -1,9 +0,0 @@
-from django.utils.importlib import import_module
-
-def get_class(name):
-    class_name = name.split('.')[-1]
-    module_name = '.'.join(name.split('.')[:-1])
-    mod = import_module(module_name)
-    cls = getattr(mod, class_name)
-    return cls
-
